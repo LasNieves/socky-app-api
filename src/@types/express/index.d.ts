6dcdfe9fdbed8d856273c2662f5de1c5913f3c37
FrancoMusolino <francomusolino55@gmail.com>
2022-11-15 12:41:07 -0300

    Fix: declaration merging request express

---
+++ src/@types/express/index.d.ts
@@ -0,0 +1,10 @@
+import * as core from 'express-serve-static-core'
+import { User } from '../../core/entities'
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User
+    }
+  }
+}
