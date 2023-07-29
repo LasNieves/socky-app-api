5cdf0c77c60f7cb87c36517e252caf051e267d2b
FrancoMusolino <francomusolino55@gmail.com>
2023-07-29 13:08:32 -0300

    (docker-compose) add DB_NAME env in docker compose

--- prisma/migrations/20221103172405_cascade_behavior/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "users_workspaces" DROP CONSTRAINT "users_workspaces_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "users_workspaces" ADD CONSTRAINT "users_workspaces_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
